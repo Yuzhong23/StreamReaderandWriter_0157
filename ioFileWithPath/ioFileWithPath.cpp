8b41674b1e9aa9170e75e8c0246be12d2d5e191c
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 10:41:01 +0700

    Membuka file dalam mode menulis dan Menunjuk ke sebuah nama file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -6,4 +6,17 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	// Membuka file dalam mode menulis.
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menuliskan file \'q\' untuk keluar" << endl;
+
+
+
 }
