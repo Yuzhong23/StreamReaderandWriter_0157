3b81b0531f08b2717ac856b29da10e79edae5369
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 10:28:38 +0700

    Melakukan perulangan setiap baris

--- StreamReaderandWriter_0157.cpp
+++ StreamReaderandWriter_0157.cpp
@@ -36,4 +36,16 @@ int main() {
 	// menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	// jika file ada maka
+	if (infile.is_open())
+	{
+		// Melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// Dan tampilkan di sini
+			cout << baris << '\n';
+		}
+	}
+
 }
