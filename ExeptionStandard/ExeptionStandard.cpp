55423dafcc8eb9efaf7cb8d070594cabe35c2cb8
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 11:05:43 +0700

    Membuat prosedure awal dari Exeption Standard

---
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek arrayt yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
