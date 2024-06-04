5197b94f29b4ec8b6f46df202aa8bf7c6ec83508
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 10:16:42 +0700

    Menunjuk ke sebuah nama file

--- StreamReaderandWriter_0157.cpp
+++ StreamReaderandWriter_0157.cpp
@@ -13,4 +13,8 @@ int main() {
 
 	// membuka file dalam node menulis.
 	ofstream outfile;
+	// Menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
