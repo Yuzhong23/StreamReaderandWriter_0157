e941a5153d332e5c8828d91177a8f8ed2bbdbaf8
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 11:11:45 +0700

    Menyelesaikan prosedure dalam ExeptionStandard

--- ExeptionStandard/ExeptionStandard.cpp
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -7,5 +7,21 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 1, 2, 3 };
+        //pesan array integer 3 elemen
+        //cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //Penangkapan menggunakan obyek exeption
+        cout << e.what() << endl;
+        //Akan dieksekusi karna array data hanya
+        //memiliki 3 elemen
+    }
+    cout << "Baris Program yang terkahir" << endl;
+    // Penanda 2: bahwa proogram berjalan tanpa berhenti
+    // Meskipun terjadi kesalahan
+    return 0;
 }
