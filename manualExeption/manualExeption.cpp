507cc0b046edab110b2a6e19c7076c808d57cc96
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-04 10:59:38 +0700

    Menyelesaikan prosedure dari manualExeption

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//throw 0.5; //Melemparkan sebuah integer maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+	catch (int a) {
+		//block ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		//Jika selain integer maka blockini akan dieksekusi
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
 }
